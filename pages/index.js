580d2afbaec4b02744128f41a2b2a6930b5d323e
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-09-15 13:00:48 +0100

    add nav and footer components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
     <div>
-      <h1>My Meals</h1>
+      <h1 className={styles.title}>My Meals</h1>
     </div>
   );
 }
