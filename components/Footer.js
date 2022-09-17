28d19e616172dbed6fcb24faafe62b61087a8089
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-09-17 13:30:04 +0100

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,17 +4,8 @@ import styles from "../styles/Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.text}>
-        Built with{" "}
-        <Link href="https://nextjs.org/">
-          <span className={styles.stack}>Nextjs</span>
-        </Link>{" "}
-        and{" "}
-        <Link href="https://supabase.com/">
-          <span className={styles.stack}>Supabase</span>
-        </Link>
-      </h1>
-      <Link href="">
+      <h1 className={styles.text}> Copyright © 2022 My Meals</h1>
+      <Link href="https://github.com/DoodleDeBug/my-meals">
         <p className={styles.repo}>Github Repo</p>
       </Link>
     </div>
