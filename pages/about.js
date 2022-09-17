414e4071df198cc925c8c90865d321781b497d1c
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-09-17 18:45:08 +0100

    add some dummy content to about page

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,16 @@ const About = () => {
   return (
     <div>
       <h1 className={styles.title}>About page</h1>
+      <p className={styles.text}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quo,
+        vero eius non at tempore officiis odio asperiores exercitationem nostrum
+        quis a omnis voluptates enim quam, veniam amet quasi. Maxime.
+      </p>
+      <p className={styles.text}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla quo,
+        vero eius non at tempore officiis odio asperiores exercitationem nostrum
+        quis a omnis voluptates enim quam, veniam amet quasi. Maxime.
+      </p>
     </div>
   );
 };
