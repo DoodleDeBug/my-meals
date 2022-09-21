ac84109efb101fbf1f0b18311b24ab212a097137
M <75613073+DoodleDeBug@users.noreply.github.com>
2022-09-21 10:27:10 +0100

    change date in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <footer>Copyright 2021 My Meals</footer>;
+  return <footer>Copyright 2022 My Meals</footer>;
 };
 
 export default Footer;
